b52deb4021d8b5460ee9941e7873dec85116adc8
Jean-Laurent Picard <jean.laurent.picard@gmail.com>
2021-10-26 10:46:13 +0200

    Initiated CSharp console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XiangQiGameConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! We are DCI!");
+        }
+    }
+}
