2d97a508200f861c24bb70e0b4cc34c9a9ecd71e
Jean-Laurent Picard <jean.laurent.picard@gmail.com>
2021-10-26 10:58:54 +0200

    To do

--- Program.cs
+++ Program.cs
@@ -7,6 +7,26 @@ namespace XiangQiGameConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! We are DCI!");
+
+            // Start writting some basic classes:
+            //    - Board
+            //    - ChessPieces
+            //       - Solider
+            //       - General
+            //       - etc...
+
+            // ChessPieces
+            //   --> Method return the name of the piece
+            //   --> piece = new Soldier()
+            //       console.WriteLine("My name is " & piece.name()) --> "My Name is Soldier"
+
+            // Basic game routine
+            //   Player 1 and player 2
+            //   Each turn, ask the player to choose a piece, and move
+
+            // Print the board ??
+            
+
         }
     }
 }
